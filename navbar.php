916b9b3df4bcbf3bf51cecf03991b714699a9a4c
Akanksha Shukla <63357474+sakanksha19@users.noreply.github.com>
2021-11-17 23:04:57 +0530

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {margin:0;}
+.nav{
+    margin-bottom:12px;
+}
+ul {
+  margin: 0;
+  padding :0;
+  overflow: hidden;
+  background-color: black;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: green;
+}
+
+</style>
+</head>
+<body>
+   <nav class="Nav">
+    <ul>
+    <li>
+    <a href="index.php">Home</a>
+    </li>
+    <li>
+    <a href="CustomersDetails.php">Customers Details</a>
+    </li>
+    <li>
+    <a href="FundTransferDetails.php">Fund Transfer Details</a>
+    </li>
+    <li>
+    <a href="TransactionDetails.php">Transaction History Details</a>
+    </li>
+    </ul>
+   </nav>
+<body>
+</html>
