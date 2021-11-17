4724a0c2f179619c7668dc0f60391e3f65503e22
Akanksha Shukla <63357474+sakanksha19@users.noreply.github.com>
2021-11-17 23:01:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Sparks Bank</title>
+  <style>
+    body {
+      padding-top: 55px;
+      font-size:23px;
+      background: blanchedalmond;
+    }
+    .center{
+      padding-top:5px;
+      background: #c0c0e6;
+      display: block;
+      margin-top: 20px;
+      margin-left: auto;
+      margin-right: auto;
+      height: 40%;
+      width: 50%;
+    }
+    footer {
+            background-color: black;
+            left: 0;
+            bottom: 0;
+            height: 120px;
+            width: 100%;
+        }
+  </style>
+</head>
+<body>
+  <?php include('navbar.php'); ?>
+  <div class="center">
+      <h1 style="text-align: center">Welcome to Sparks Bank<h1>
+      <img class ="center" src="bankImage.png" alt="" width="150" height="300">
+      <p style="text-align: center ; font-size: 15px"><i>"Good Banking is produced not by good laws, but by good bankers."</i><p>
+     
+  </div>
+  <footer style="padding-top: 2px; text-align: center; color: white">
+    <p style="font-size: 15px">Designed by: Akanksha Shukla</p>
+    <p style="font-size: 15px">Contact Number: 8303.....X</p>
+    <p style="font-size: 15px">E-mail: aka......@gmail.com</p> 
+</footer>
+</body>
+</html>
