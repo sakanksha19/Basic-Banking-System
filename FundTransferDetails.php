9adf66178153044bdd587133a13a5f4d0c9866a4
Akanksha Shukla <63357474+sakanksha19@users.noreply.github.com>
2021-11-18 08:37:23 +0530

    Update FundTransferDetails.php

--- FundTransferDetails.php
+++ FundTransferDetails.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "";
-    $username = ""; 
-    $password = ""; 
-    $dname = ""; 
+    $servername = "fdb34.awardspace.net";
+    $username = "3986264_akanksha19"; 
+    $password = "u-nudn587BdR72a}"; 
+    $dname = "3986264_akanksha19"; 
     $con = new mysqli($servername, $username, $password, $dname); 
     if ($con->connect_error) { 
     die("Connection failed: " . $con->connect_error); 
@@ -38,6 +38,7 @@
     </script>
 </head>
 <body>
+<img src="bankinghub.jpg" alt="bank" style= "width:100%; height: 92%; position: absolute; z-index:-1; opacity: 0.7">
 <?php include('navbar.php'); ?>
 <div class = 'moneyTransfer'>
     <h1>Money Transfer</h1>
